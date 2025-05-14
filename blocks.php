43329cc533661119c429a4363ec83c64b28719d1
mohammedayaz24 <147026092+mohammedayaz24@users.noreply.github.com>
2025-05-14 12:38:07 +0530

    Update blocks.php

--- blocks.php
+++ blocks.php
@@ -104,8 +104,8 @@ if (!isset($_SESSION['username'])) {
     <div class="block"><a href="eee_block.php" style="text-decoration:none; color: inherit;">EEE BLOCK</a></div>    
     <div class="block"><a href="mba_block.php" style="text-decoration:none; color: inherit;">MBA BLOCK</a></div>    
     <div class="block"><a href="mech_block.php" style="text-decoration:none; color: inherit;">MECH BLOCK</a></div>    
-    <div class="block"><a href="aero_block.php" style="text-decoration:none; color: inherit;">MECH BLOCK</a></div>    
-    <div class="block">ARCHITECT BLOCK</div>
+    <div class="block"><a href="aero_block.php" style="text-decoration:none; color: inherit;">AERO BLOCK</a></div>    
+    <div class="block"><a href="arch_block.php" style="text-decoration:none; color: inherit;">ARCHITECT BLOCK</a></div>    
     <div class="block"><a href="BOYS_HOSTEL.php" style="text-decoration:none; color: inherit;">BOYS HOSTEL</a></div>     
     <div class="block">GIRLS HOSTEL</div>
     <div class="block"><a href="canteen.php" style="text-decoration:none; color: inherit;">MAIN CANTEEN</a></div>     
