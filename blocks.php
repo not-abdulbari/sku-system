8c3965a92d08684a14c31b739a3e025a149bb3a2
aasif-create <147026508+aasif-create@users.noreply.github.com>
2025-05-14 12:39:53 +0530

    Update blocks.php

--- blocks.php
+++ blocks.php
@@ -107,7 +107,7 @@ if (!isset($_SESSION['username'])) {
     <div class="block"><a href="BOYS_HOSTEL.php" style="text-decoration:none; color: inherit;">BOYS HOSTEL</a></div>     
     <div class="block"><a href="GIRLS_HOSTEL.php" style="text-decoration:none; color: inherit;">GIRLS HOSTEL</a></div>     
     <div class="block">MAIN CANTEEN</div>
-    <div class="block">MINI CANTEEN</div>
+    <div class="block"><a href="MINI_CANTEEN.php" style="text-decoration:none; color: inherit;">MINI CANTEEN</a></div>    
     <div class="block">GUEST ROOM</div>
 
 
